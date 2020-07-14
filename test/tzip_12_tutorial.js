b5e3246a367066a5c8eac2e537528866b75d9a2f
Thorkil Vaerge <thorkil.vaerge@inacta.ch>
2020-07-14 22:25:14 +0200

    Add unit tests of update_operators endpoint

--- test/tzip_12_tutorial.js
+++ test/tzip_12_tutorial.js
@@ -17,134 +17,223 @@ contract('tzip_12_tutorial', accounts => {
         /**
          * Display the current contract address for debugging purposes
          */
-        console.log('Contract deployed at:', tzip_12_tutorial_instance.address);
+        // console.log('Contract deployed at:', tzip_12_tutorial_instance.address);
         storage = await tzip_12_tutorial_instance.storage();
     });
 
-    it('should be able to add an operator', async () => {
-        const tokenOwner = alice.pkh;
-        const accountBefore = await storage.get(tokenOwner);
-        assert.equal(0, accountBefore.allowances.length);
-        assert.equal(true, Array.isArray(accountBefore.allowances)); // I couldn't find an `assert.true`
+    describe('update operators', () => {
+        describe('add operator', () => {
+            it('should be able to add an operator and send when added', async () => {
+                const tokenOwner = alice.pkh;
+                const accountBefore = await storage.get(tokenOwner);
+                assert.equal(true, Array.isArray(accountBefore.allowances)); // I couldn't find an `assert.true`
 
-        const tokenOperator = bob.pkh;
-        await tzip_12_tutorial_instance.update_operators([{
-            'add_operator': {
-                owner: tokenOwner,
-                operator: tokenOperator
-            }
-        }]);
-        const accountAfter = await storage.get(tokenOwner);
-        assert.equal(1, accountAfter.allowances.length);
-        assert.equal(true, Array.isArray(accountAfter.allowances)); // I couldn't find an `assert.true`
+                const tokenOperator = bob.pkh;
+                await tzip_12_tutorial_instance.update_operators([{
+                    'add_operator': {
+                        owner: tokenOwner,
+                        operator: tokenOperator
+                    }
+                }]);
+                const accountAfter = await storage.get(tokenOwner);
+                assert.equal(1, accountAfter.allowances.length);
+                assert.equal(true, Array.isArray(accountAfter.allowances)); // I couldn't find an `assert.true`
+
+                // TODO: Verify that bob can now spend from Alice's account
+            });
+
+            it('adding an operator should be an idempotent operation', async () => {
+                const tokenOwner = alice.pkh;
+
+                const tokenOperator = bob.pkh;
+                const updateParam = [{
+                    'add_operator': {
+                        owner: tokenOwner,
+                        operator: tokenOperator
+                    }
+                }];
+
+                // Call update_operators twice and ensure that only one entry has been made
+                const prom = tzip_12_tutorial_instance.update_operators(updateParam);
+                await tzip_12_tutorial_instance.update_operators(updateParam);
+                prom.then(() => {
+                    storage.get(tokenOwner).then(accountAfter => {
+                        assert.equal(1, accountAfter.allowances.length);
+                        assert.equal(tokenOperator, accountAfter.allowances[0]);
+                    });
+                });
+
+                // Call it again and ensure that nothing has changed
+                await tzip_12_tutorial_instance.update_operators(updateParam);
+                storage.get(tokenOwner).then(accountAfter => {
+                    assert.equal(1, accountAfter.allowances.length);
+                    assert.equal(tokenOperator, accountAfter.allowances[0]);
+                });
+            });
+
+            it('should be able to add and remove multiple allowances in one call', async () => {
+                const tokenOwner = alice.pkh;
+                const tokenOperator0 = bob.pkh;
+                const tokenOperator1 = 'tz1PueLmqFpGmSNboPXvxDs6Th49xKwJfNAQ'; // random address found on Carthage net
+                const addParam = [
+                    {
+                        'add_operator': {
+                            owner: tokenOwner,
+                            operator: tokenOperator0
+                        }
+                    },
+                    {
+                        'add_operator': {
+                            owner: tokenOwner,
+                            operator: tokenOperator1
+                        }
+                    }
+                ];
+                await tzip_12_tutorial_instance.update_operators(addParam);
+
+                storage.get(tokenOwner).then(accountAfter => {
+                    assert.equal(2, accountAfter.allowances.length);
+                    assert.equal(true, accountAfter.allowances.includes(tokenOperator0));
+                    assert.equal(true, accountAfter.allowances.includes(tokenOperator1));
+                });
+
+                const removeParam = [
+                    {
+                        'remove_operator': {
+                            owner: tokenOwner,
+                            operator: tokenOperator0
+                        }
+                    },
+                    {
+                        'remove_operator': {
+                            owner: tokenOwner,
+                            operator: tokenOperator1
+                        }
+                    }
+                ];
+                await tzip_12_tutorial_instance.update_operators(removeParam);
+                storage.get(tokenOwner).then(accountAfter => {
+                    assert.equal(0, accountAfter.allowances.length);
+                });
+            });
+        });
+
+        describe('remove operator', () => {
+            it('should be able to add and remove an operator', async () => {
+                const tokenOwner = alice.pkh;
+
+                const tokenOperator = bob.pkh;
+                await tzip_12_tutorial_instance.update_operators([{
+                    'add_operator': {
+                        owner: tokenOwner,
+                        operator: tokenOperator
+                    }
+                }]);
+                const accountMid = await storage.get(tokenOwner);
+                assert.equal(1, accountMid.allowances.length);
+                assert.equal(true, Array.isArray(accountMid.allowances));
+                assert.equal(tokenOperator, accountMid.allowances[0]);
+
+                const removeParam = [{
+                    'remove_operator': {
+                        owner: tokenOwner,
+                        operator: tokenOperator
+                    }
+                }];
+                await tzip_12_tutorial_instance.update_operators(removeParam);
+                const accountAfter = await storage.get(tokenOwner);
+                assert.equal(0, accountAfter.allowances.length);
+
+                // Removing an operator should be an idempotent operation
+                await tzip_12_tutorial_instance.update_operators(removeParam);
+                const accountAfterAfter = await storage.get(tokenOwner);
+                assert.equal(0, accountAfterAfter.allowances.length);
+            });
+        });
     });
 
-    const expectedBalanceAlice = initial_storage.get(alice.pkh).balance;
-    it(`should store a balance of ${expectedBalanceAlice} for Alice`, async () => {
-        /**
-         * Get balance for Alice from the smart contract's storage (by a big map key)
-         */
-        const deployedBalanceAlice = (await storage.get(alice.pkh)).balance;
-        assert.equal(expectedBalanceAlice, deployedBalanceAlice);
-    });
-
-    it(`should not store any balance for Bob`, async () => {
-        let accountBob = await storage.get(bob.pkh);
-        assert.equal(accountBob, undefined);
-    });
-
-    it('should transfer 1 token from Alice to Bob', async () => {
-        const transferParam = [
-            {
-                /**
-                 * token_id: 0 represents the single token_id within our contract
-                 */
-                token_id: 0,
-                amount: 1,
-                from_: alice.pkh,
-                to_: bob.pkh
-            }
-        ];
-
-        /**
-         * Call the `transfer` entrypoint
-         */
-        await tzip_12_tutorial_instance.transfer(transferParam);
-        /**
-         * Bob's token balance should now be 1 and Alice's 9.
-         */
-        const deployedAccountBob = await storage.get(bob.pkh);
-        const expectedBalanceBob = 1;
-        assert.equal(deployedAccountBob.balance, expectedBalanceBob);
-
-        const deployedAccountAlice = await storage.get(alice.pkh);
-        const expectedBalanceAlice = 9;
-        assert.equal(deployedAccountAlice.balance, expectedBalanceAlice);
-    });
-
-    it(`should not allow transfers from_ an address that did not sign the transaction`, async () => {
-        const transferParam = [
-            {
-                token_id: 0,
-                amount: 1,
-                from_: bob.pkh,
-                to_: alice.pkh
-            }
-        ];
-
-        try {
+    describe('update operators', () => {
+        const expectedBalanceAlice = initial_storage.get(alice.pkh).balance;
+        it(`should store a balance of ${expectedBalanceAlice} for Alice`, async () => {
             /**
-             * Transactions in the test suite are signed by a secret/private key
-             * configured in truffle-config.js
+             * Get balance for Alice from the smart contract's storage (by a big map key)
+             */
+            const deployedBalanceAlice = (await storage.get(alice.pkh)).balance;
+            assert.equal(expectedBalanceAlice, deployedBalanceAlice);
+        });
+
+        it(`should not store any balance for Bob`, async () => {
+            let accountBob = await storage.get(bob.pkh);
+            assert.equal(accountBob, undefined);
+        });
+
+        it('should transfer 1 token from Alice to Bob', async () => {
+            const transferParam = [
+                {
+                    /**
+                     * token_id: 0 represents the single token_id within our contract
+                     */
+                    token_id: 0,
+                    amount: 1,
+                    from_: alice.pkh,
+                    to_: bob.pkh
+                }
+            ];
+
+            /**
+             * Call the `transfer` entrypoint
              */
             await tzip_12_tutorial_instance.transfer(transferParam);
-        } catch (e) {
-            assert.equal(e.message, constants.contractErrors.fromEqualToSenderAddress)
-        }
-    });
+            /**
+             * Bob's token balance should now be 1 and Alice's 9.
+             */
+            const deployedAccountBob = await storage.get(bob.pkh);
+            const expectedBalanceBob = 1;
+            assert.equal(deployedAccountBob.balance, expectedBalanceBob);
 
-    it(`should not transfer tokens from Alice to Bob when Alice's balance is insufficient`, async () => {
-        const transferParam = [
-            {
-                token_id: 0,
-                // Alice's balance at this point is 9
-                amount: 100,
-                from_: alice.pkh,
-                to_: bob.pkh
+            const deployedAccountAlice = await storage.get(alice.pkh);
+            const expectedBalanceAlice = 9;
+            assert.equal(deployedAccountAlice.balance, expectedBalanceAlice);
+        });
+
+        it(`should not allow transfers from_ an address that did not sign the transaction`, async () => {
+            const transferParam = [
+                {
+                    token_id: 0,
+                    amount: 1,
+                    from_: bob.pkh,
+                    to_: alice.pkh
+                }
+            ];
+
+            try {
+                /**
+                 * Transactions in the test suite are signed by a secret/private key
+                 * configured in truffle-config.js
+                 */
+                await tzip_12_tutorial_instance.transfer(transferParam);
+            } catch (e) {
+                assert.equal(e.message, constants.contractErrors.fromEqualToSenderAddress)
             }
-        ];
+        });
 
-        try {
-            await tzip_12_tutorial_instance.transfer(transferParam);
-        } catch (e) {
-            assert.equal(e.message, constants.contractErrors.insufficientBalance)
-        }
+        it(`should not transfer tokens from Alice to Bob when Alice's balance is insufficient`, async () => {
+            const transferParam = [
+                {
+                    token_id: 0,
+                    // Alice's balance at this point is 9
+                    amount: 100,
+                    from_: alice.pkh,
+                    to_: bob.pkh
+                }
+            ];
+
+            try {
+                await tzip_12_tutorial_instance.transfer(transferParam);
+            } catch (e) {
+                assert.equal(e.message, constants.contractErrors.insufficientBalance)
+            }
+        });
+
     });
-
-    // For some reason the type of the function argument is not correct here
-    // it('Should not be able to get other balances than for token ID 0', async () => {
-    //     const balanceOfReqs = [
-    //         {
-    //             token_id: 1,
-    //             owner: alice.pkh,
-    //         }
-    //     ];
-
-    //     const balanceOfParam = {
-    //         requests: balanceOfReqs,
-    //         callback: tzip_12_tutorial_instance,
-    //     };
-
-    //     try {
-    //         await tzip_12_tutorial_instance.balance_of(balanceOfParam);
-    //     } catch (e) {
-    //         assert.equal(e.message, constants.contractErrors.unknownTokenId)
-    //     }
-    // });
-
-    // it('should be possible to call balance_of endpoint', async () => {
-    //     const balanceOfParam = [
-
-    //     ]
-    // })
 });
